dc80dbe689faba35aff8a9dd1ddbd717ef1c39e1
Adam Howell <adam.dhowell.19@gmail.com>
2021-02-08 17:17:37 -0600

    Added some logic and a test with my own data

--- testing/test_suite.py
+++ testing/test_suite.py
@@ -122,7 +122,7 @@ class TestTrueAccordTestCode(unittest.TestCase):
         
     def test_payment_plan_association(self):
         debts_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts")
-        pyament_plans_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
+        payment_plans_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
         
         debts_json = [
           {
@@ -152,12 +152,13 @@ class TestTrueAccordTestCode(unittest.TestCase):
           }
         ]
         
-        debts_data = working.is_associated_with_payment_plan(debts_data, pyament_plans_data)
+        debts_data = working.is_associated_with_payment_plan(debts_data, payment_plans_data)
         self.assertEqual(debts_json, debts_data)
     
     def test_next_payment_calculation(self):
         debts_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts")
-        pyament_plans_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
+        payment_plans_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
+        payments_data = working.get_data("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payments")
         
         debts_json = [
           {
@@ -171,15 +172,15 @@ class TestTrueAccordTestCode(unittest.TestCase):
             "amount": 100,
             "id": 1,
             'is_in_payment_plan': True,
-            'remaining_amount': 75, 
-            'next_payment_due_date': '2020-08-08 00:00:00'
+            'remaining_amount': 50, 
+            'next_payment_due_date': '2020-08-08'
           },
           {
             "amount": 4920.34,
             "id": 2,
             'is_in_payment_plan': True,
-            'remaining_amount': 3690.255, 
-            'next_payment_due_date': '2020-01-15 00:00:00'
+            'remaining_amount': 4920.34, 
+            'next_payment_due_date': '2020-01-15'
           },
           {
             "amount": 12938,
@@ -197,6 +198,149 @@ class TestTrueAccordTestCode(unittest.TestCase):
           }
         ]
         
-        debts_data = working.is_associated_with_payment_plan(debts_data, pyament_plans_data)
-        debts_data = working.next_payment(debts_data, pyament_plans_data)
+        debts_data = working.is_associated_with_payment_plan(debts_data, payment_plans_data)
+        debts_data = working.next_payment(debts_data, payment_plans_data, payments_data)
+        self.assertEqual(debts_json, debts_data)
+        
+    def test_next_payment_calculation_own_data(self):
+        debts_data = [
+          {
+            "amount": 123.46,
+            "id": 0
+          },
+          {
+            "amount": 100,
+            "id": 1
+          },
+          {
+            "amount": 4920.34,
+            "id": 2
+          },
+          {
+            "amount": 1230.08,
+            "id": 3
+          },
+          {
+            "amount": 9238.02,
+            "id": 4
+          }
+        ]
+        
+        payment_plans_data = [
+          {
+            "amount_to_pay": 102.5,
+            "debt_id": 0,
+            "id": 0,
+            "installment_amount": 51.25,
+            "installment_frequency": "WEEKLY",
+            "start_date": "2020-09-28"
+          },
+          {
+            "amount_to_pay": 100,
+            "debt_id": 1,
+            "id": 1,
+            "installment_amount": 25,
+            "installment_frequency": "WEEKLY",
+            "start_date": "2020-08-01"
+          },
+          {
+            "amount_to_pay": 4920.34,
+            "debt_id": 2,
+            "id": 2,
+            "installment_amount": 1230.085,
+            "installment_frequency": "BI_WEEKLY",
+            "start_date": "2020-01-01"
+          },
+          {
+            "amount_to_pay": 1230.08,
+            "debt_id": 3,
+            "id": 3,
+            "installment_amount": 1230.08,
+            "installment_frequency": "WEEKLY",
+            "start_date": "2020-08-01"
+          }
+        ]
+        payments_data = [
+          {
+            "amount": 51.25,
+            "date": "2020-09-29",
+            "payment_plan_id": 0
+          },
+          {
+            "amount": 51.25,
+            "date": "2020-10-29",
+            "payment_plan_id": 0
+          },
+          {
+            "amount": 25,
+            "date": "2020-08-08",
+            "payment_plan_id": 1
+          },
+          {
+            "amount": 25,
+            "date": "2020-08-08",
+            "payment_plan_id": 1
+          },
+          {
+            "amount": 4312.67,
+            "date": "2020-08-08",
+            "payment_plan_id": 2
+          },
+          {
+            "amount": 1230.085,
+            "date": "2020-08-01",
+            "payment_plan_id": 3
+          },
+          {
+            "amount": 1230.085,
+            "date": "2020-08-08",
+            "payment_plan_id": 3
+          },
+          {
+            "amount": 1230.085,
+            "date": "2020-08-15",
+            "payment_plan_id": 3
+          }
+        ]
+        
+        debts_json = [
+          {
+            "amount": 123.46,
+            "id": 0,
+            'is_in_payment_plan': False,
+            'remaining_amount': None, 
+            'next_payment_due_date': None
+          },
+          {
+            "amount": 100,
+            "id": 1,
+            'is_in_payment_plan': True,
+            'remaining_amount': 50, 
+            'next_payment_due_date': '2020-08-08'
+          },
+          {
+            "amount": 4920.34,
+            "id": 2,
+            'is_in_payment_plan': True,
+            'remaining_amount': 4920.34, 
+            'next_payment_due_date': '2020-01-15'
+          },
+          {
+            "amount": 1230.08,
+            "id": 3,
+            'is_in_payment_plan': True,
+            'remaining_amount': None, 
+            'next_payment_due_date': None
+          },
+          {
+            "amount": 9238.02,
+            "id": 4,
+            'is_in_payment_plan': False,
+            'remaining_amount': None, 
+            'next_payment_due_date': None
+          }
+        ]
+        
+        debts_data = working.is_associated_with_payment_plan(debts_data, payment_plans_data)
+        debts_data = working.next_payment(debts_data, payment_plans_data, payments_data)
         self.assertEqual(debts_json, debts_data)
--- work/working.py
+++ work/working.py
@@ -11,27 +11,40 @@ def print_data(data):
     for anything in data:
         print(anything)
         
-def next_payment(debt_data_dict, payment_plan_data_dict):
+def next_payment(debt_data_dict, payment_plan_data_dict, payment_data_dict):
     for debt_data in debt_data_dict:
         is_in_payment_plan = debt_data.get('is_in_payment_plan')
         remaining_amount = None
         next_payment_due_date_str = None
+        
         if is_in_payment_plan:
+            debt_id = debt_data.get('id')
+            
             for payment_plan_data in payment_plan_data_dict:
-                if debt_data.get('id') == payment_plan_data.get('debt_id'):
+                if debt_id == payment_plan_data.get('debt_id'):
+                    payment_plan_id = payment_plan_data.get('debt_id')
                     amount_to_pay = payment_plan_data.get('amount_to_pay')
-                    installment_amount = payment_plan_data.get('installment_amount')
-                    remaining_amount = amount_to_pay - installment_amount
                     installment_frequency = payment_plan_data.get('installment_frequency')
                     next_payment_value = 7 if installment_frequency == 'WEEKLY' else 14
                     start_date_str = payment_plan_data.get('start_date')
                     start_payment_date = parser.parse(start_date_str)
                     next_payment_date = start_payment_date + timedelta(days=next_payment_value)
-                    next_payment_due_date_str = next_payment_date.strftime("%Y-%m-%d %H:%M:%S")
+                    next_payment_due_date_str = next_payment_date.strftime("%Y-%m-%d")
+                    remaining_amount = amount_to_pay
                     
-                    if amount_to_pay <= 0.0:
-                        remaining_amount = None
-                        next_payment_due_date_str = None
+                    for payment_data in payment_data_dict:
+                        if payment_plan_id == payment_data.get('payment_plan_id'):
+                            payment_date = parser.parse(payment_data.get('date'))
+                            if payment_date <= next_payment_date:
+                                amount_paid = payment_data.get('amount')
+                                remaining_amount -= amount_paid
+                                
+                                if remaining_amount <= 0.0:
+                                    remaining_amount = None
+                                    next_payment_due_date_str = None
+                                    break
+                    
+                    
                         
         debt_data.update({'remaining_amount':remaining_amount, 'next_payment_due_date':next_payment_due_date_str})
     return debt_data_dict
@@ -41,6 +54,7 @@ def is_associated_with_payment_plan(data1, data2):
         is_in_payment_plan = False
         debt_id = debt_data.get('id')
         debt_amount = debt_data.get('amount')
+        
         for payment_plan in data2:
             if debt_id == payment_plan.get('debt_id') and debt_amount == payment_plan.get('amount_to_pay'):
                 is_in_payment_plan = True
@@ -59,8 +73,11 @@ def main(debt_url, payment_plan_url, payments_url):
     payments_data = get_data(payments_url)
     
     debt_data = is_associated_with_payment_plan(debt_data, payment_plan_data)
-    debt_data = next_payment(debt_data, payment_plan_data)
+    debt_data = next_payment(debt_data, payment_plan_data, payments_data)
     print_data(debt_data)
+    
+    print(payment_plan_data)
+    print(payments_data)
     
     
 #main("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts", 
